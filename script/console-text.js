aa9725673baa2f122a5caaa35bb6939addeccfec
valeriyanechy <valeriyanechyporuk@gmail.com>
2024-10-15 12:14:23 +0200

    console changes

--- script/console-text.js
+++ script/console-text.js
@@ -1,13 +1,13 @@
-// // Custom styling function
-// function logWithStyle(message, styles) {
-//     console.log('%c' + message, styles.join(';'));
-// }
+// Custom styling function
+function logWithStyle(message, styles) {
+    console.log('%c' + message, styles.join(';'));
+}
 
-// // Styled messages
-// logWithStyle('Oh hey there! You are a curious one, huh...', ['color: #2E68F6', 'font-size: 30px', 'font-weight: bold', 'background-color: #FDF9EF', 'padding: 20px']);
-// logWithStyle('It is totally fine! Welcome to my portfolio!', ['color: #2E68F6', 'font-size: 16px', 'padding: 20px', 'background-color: #FFF3D9']);
+// Styled messages
+logWithStyle('Oh hey there! You are a curious one, huh...', ['color: #2E68F6', 'font-size: 30px', 'font-weight: bold', 'background-color: #FDF9EF', 'padding: 20px']);
+logWithStyle('It is totally fine! Welcome to my portfolio!', ['color: #2E68F6', 'font-size: 16px', 'padding: 20px', 'background-color: #FFF3D9']);
 
-// Block of repeated characters
+
 const block = `
 VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
@@ -31,10 +31,6 @@ VVVVVVVVVVVVVVVVVVnnnnnnnnnnnVVVVVVVVVVVVVVVVVVV
 VVVVVVVVVVVVVVVVVVVnnnnnnnnnVVVVVVVVVVVVVVVVVVVV
 VVVVVVVVVVVVVVVVVVVVnnnnnnnVVVVVVVVVVVVVVVVVVVVV
 VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-`;  // (Truncate or generate more lines as needed)
+`;
+console.log(block);
 
-console.log(block);  // Log the block to the console
-
-console.log(
-    'Oh, so you are a curious one, huh? Its totally fine, I am too! Welcome to my portfolio :)'
-);
